49706f328e100eb1607a0c0ca0b41c931e90d520
Tyler <timeisttyler@gmail.com>
2024-10-01 16:03:28 -0400

    plugin did not like that i was doing runTaskAsync while it was being disabled; minor complaint. just added a try-catch that does nothing

--- src/main/java/com/lum/minecraft/listeners/DiscordChannelListener.java
+++ src/main/java/com/lum/minecraft/listeners/DiscordChannelListener.java
@@ -10,6 +10,7 @@ import github.scarsz.discordsrv.dependencies.jda.api.hooks.ListenerAdapter;
 import github.scarsz.discordsrv.util.DiscordUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.IllegalPluginAccessException;
 
 public class DiscordChannelListener extends ListenerAdapter {
 
@@ -18,47 +19,48 @@ public class DiscordChannelListener extends ListenerAdapter {
     @Subscribe
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        try {
+            Bukkit.getScheduler().runTaskAsynchronously(LumsDekos.getInstance(), () -> {
+                Bukkit.getOnlinePlayers().forEach((target) -> {
 
-        Bukkit.getScheduler().runTaskAsynchronously(LumsDekos.getInstance(), () -> {
-            Bukkit.getOnlinePlayers().forEach((target) -> {
-
-                PlayerFile pf = new PlayerFile(target.getUniqueId());
-                if (pf.getConfig().getString("connectedchannel").equalsIgnoreCase(event.getChannel().getId())) {
-
-
-                    Member m = DiscordUtil.getJda().getGuilds().get(0).getMember(DiscordUtil.getJda().getUserById(DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(target.getUniqueId())));
-
-                    Member m2 = DiscordUtil.getJda().getGuilds().get(0).getMember(event.getAuthor());
-
-                    //If the channel is a staff channel, do XYZ to ensure that they actually have access.
-                    if (DiscordUtil.getJda().getTextChannelById(pf.getConfig().getString("connectedchannel")).canTalk(m)) {
-
-                        if (!event.getMessage().getAuthor().getName().equalsIgnoreCase(target.getName() + " // unity.exousia.online")) {
+                    PlayerFile pf = new PlayerFile(target.getUniqueId());
+                    if (pf.getConfig().getString("connectedchannel").equalsIgnoreCase(event.getChannel().getId())) {
 
 
-                            if (pf.getConfig().get("listen-mode").equals("bot-only") && event.getAuthor().isBot())
-                                target.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&l" + event.getMessage().getAuthor().getName() + " &7&l>> &f" + event.getMessage().getContentRaw()));
+                        Member m = DiscordUtil.getJda().getGuilds().get(0).getMember(DiscordUtil.getJda().getUserById(DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(target.getUniqueId())));
 
-                            if (pf.getConfig().get("listen-mode").equals("all")) {
-                                if (event.getAuthor().isBot())
+                        Member m2 = DiscordUtil.getJda().getGuilds().get(0).getMember(event.getAuthor());
+
+                        //If the channel is a staff channel, do XYZ to ensure that they actually have access.
+                        if (DiscordUtil.getJda().getTextChannelById(pf.getConfig().getString("connectedchannel")).canTalk(m)) {
+
+                            if (!event.getMessage().getAuthor().getName().equalsIgnoreCase(target.getName() + " // unity.exousia.online")) {
+
+
+                                if (pf.getConfig().get("listen-mode").equals("bot-only") && event.getAuthor().isBot())
                                     target.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&l" + event.getMessage().getAuthor().getName() + " &7&l>> &f" + event.getMessage().getContentRaw()));
-                                else
+
+                                if (pf.getConfig().get("listen-mode").equals("all")) {
+                                    if (event.getAuthor().isBot())
+                                        target.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3&l" + event.getMessage().getAuthor().getName() + " &7&l>> &f" + event.getMessage().getContentRaw()));
+                                    else
+                                        target.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a&l" + event.getMessage().getAuthor().getName() + " &2&l//&a&l " + m2.getNickname() + "&7&l >> &f" + event.getMessage().getContentRaw()));
+                                }
+
+                                if (pf.getConfig().get("listen-mode").equals("users") && !event.getAuthor().isBot())
                                     target.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a&l" + event.getMessage().getAuthor().getName() + " &2&l//&a&l " + m2.getNickname() + "&7&l >> &f" + event.getMessage().getContentRaw()));
                             }
 
-                            if (pf.getConfig().get("listen-mode").equals("users") && !event.getAuthor().isBot())
-                                target.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a&l" + event.getMessage().getAuthor().getName() + " &2&l//&a&l " + m2.getNickname() + "&7&l >> &f" + event.getMessage().getContentRaw()));
+                        } else {
+                            pf.getConfig().set("connectedchannel", "&");
+                            pf.save();
                         }
-
-                    } else {
-                        pf.getConfig().set("connectedchannel", "&");
-                        pf.save();
                     }
-                }
+                });
             });
-        });
 
 
-        }
+        } catch (IllegalPluginAccessException e) {}
+    }
 
 }
